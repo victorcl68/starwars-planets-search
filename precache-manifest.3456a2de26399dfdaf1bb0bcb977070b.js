74081eda686551743585ab5813ec171b0445456a
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-10 17:06:14 -0300

    Updates

---
+++ precache-manifest.3456a2de26399dfdaf1bb0bcb977070b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898462b05ad994267ce20841ca346bc2",
+    "url": "/starwars-planets-search/index.html"
+  },
+  {
+    "revision": "194ed7c0f2acac4d4774",
+    "url": "/starwars-planets-search/static/js/2.04ea3753.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets-search/static/js/2.04ea3753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c077abf8be74c8b4a9bb",
+    "url": "/starwars-planets-search/static/js/main.70642daf.chunk.js"
+  },
+  {
+    "revision": "1b9e9e3eb9707d3157b0",
+    "url": "/starwars-planets-search/static/js/runtime-main.d5e8347d.js"
+  }
+]);
